14c8496023e16939b908211b08d1f61af4a37011
Anastasia Kotova <anastasiakotova.job@gmail.com>
2024-02-12 17:51:20 +0300

    log

--- src/Helper.js
+++ src/Helper.js
@@ -18,6 +18,7 @@ export default class Helper {
   parse() {
     this._parsed = parseString(this._contents)
     logger.info('parsed:', this.parsed)
+    console.log(this._parsed);
     return this._parsed
   }
 
